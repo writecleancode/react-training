40d26e4a041fcfaf6ae758b000065c58527cf2a4
Rafal <rafek131@wp.pl>
2023-09-07 13:14:28 +0200

    Added Global Styles

--- src/views/Root.js
+++ src/views/Root.js
@@ -2,10 +2,12 @@ import { cars } from 'data/carsData';
 import { CarCard } from 'components/molecules/CarCard/CarCard';
 import { StyledTitle } from 'components/atoms/Title/StyledTitle';
 import { Wrapper } from './Root.styles';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
 
 const App = () => {
 	return (
 		<>
+			<GlobalStyle />
 			<StyledTitle>Segment C Hatchback cars (late 1990's, early 2000's)</StyledTitle>
 			<Wrapper>
 				{cars.map(car => {
