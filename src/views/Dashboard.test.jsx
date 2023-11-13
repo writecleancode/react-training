c926c94f7dff4dede9208a3b87d96bd30aae4deb
Rafal <rafek131@wp.pl>
2023-11-13 18:27:13 +0100

    Fixed 'Dashboard' component tests

--- src/views/Dashboard.test.jsx
+++ src/views/Dashboard.test.jsx
@@ -1,14 +1,28 @@
 import { fireEvent, render, screen } from 'src/test-utils';
 import '@testing-library/jest-dom';
+import { setupServer } from 'msw/node';
+import { handlers } from 'src/mocks/handlers';
 import { BrowserRouter } from 'react-router-dom';
 import { MainTemplate } from 'src/components/templates/MainTemplate';
 import { Dashboard } from './Dashboard';
 
+const server = setupServer(...handlers);
+
 describe('Dashboard', () => {
-	it('Renders the component', () => {
+	beforeAll(() => {
+		server.listen();
+	});
+	afterEach(() => {
+		server.resetHandlers();
+	});
+	afterAll(() => {
+		server.close();
+	});
+
+	it('Renders the component', async () => {
 		render(<Dashboard />);
 		screen.getByPlaceholderText('find car');
-		screen.getByText('Volkswagen Golf');
+		await screen.findByText('Volkswagen Golf');
 	});
 
 	it('Displays only matching cars is searchPhrase is present', () => {
@@ -20,25 +34,25 @@ describe('Dashboard', () => {
 		expect(presentCar).toBeInTheDocument();
 	});
 
-	it('Displays only matching cars if production years filters are matching', () => {
+	it('Displays only matching cars if production years filters are matching', async () => {
 		render(
-			<>
-				<BrowserRouter>
-					<MainTemplate>
-						<Dashboard />
-					</MainTemplate>
-				</BrowserRouter>
-			</>
+			<BrowserRouter>
+				<MainTemplate>
+					<Dashboard />
+				</MainTemplate>
+			</BrowserRouter>
 		);
 		const absentCar = screen.getByText('Ford Focus');
 		const presentCar = screen.getByText('Volkswagen Golf');
-		screen.getByText('1997');
+		await screen.findByTestId('1997');
 		fireEvent.click(screen.getByTestId('1997'));
 		expect(absentCar).not.toBeInTheDocument();
 		expect(presentCar).toBeInTheDocument();
+
+		fireEvent.click(screen.getByTestId('1997'));
 	});
 
-	it('Displays only matching cars if brand filters are matching', () => {
+	it('Displays only matching cars if brand filters are matching', async () => {
 		render(
 			<BrowserRouter>
 				<MainTemplate>
@@ -48,12 +62,15 @@ describe('Dashboard', () => {
 		);
 		const absentCar = screen.getByText('Ford Focus');
 		const presentCar = screen.getByText('Volkswagen Golf');
+		await screen.findByTestId('Volkswagen');
 		fireEvent.click(screen.getByTestId('Volkswagen'));
 		expect(absentCar).not.toBeInTheDocument();
 		expect(presentCar).toBeInTheDocument();
+
+		fireEvent.click(screen.getByTestId('Volkswagen'));
 	});
 
-	it('Displays only cars matching search phrase, years filters and brand filters', () => {
+	it('Displays only cars matching search phrase, years filters and brand filters', async () => {
 		render(
 			<BrowserRouter>
 				<MainTemplate>
@@ -63,11 +80,16 @@ describe('Dashboard', () => {
 		);
 		const absentCar = screen.getByText('Ford Focus');
 		const presentCar = screen.getByText('Volkswagen Golf');
+		await screen.findByTestId('1997');
+		await screen.findByTestId('Volkswagen');
 		fireEvent.click(screen.getByTestId('1997'));
 		fireEvent.click(screen.getByTestId('Volkswagen'));
 		fireEvent.change(screen.getByPlaceholderText('find car'), { target: { value: 'go' } });
 		expect(absentCar).not.toBeInTheDocument();
 		expect(presentCar).toBeInTheDocument();
+
+		fireEvent.click(screen.getByTestId('1997'));
+		fireEvent.click(screen.getByTestId('Volkswagen'));
 	});
 
 	it('Removes car from the list', () => {
